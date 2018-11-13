49285973e0643126fd9699a9a5fc8650d2166a7e
YuKongEr <541130126@qq.com>
2018-11-13 15:56:38 +0800

    :art: 修改oauth2自定义异常配置

--- panda-auth/src/main/java/com/yukong/panda/auth/handler/CustomWebResponseExceptionTranslator.java
+++ panda-auth/src/main/java/com/yukong/panda/auth/handler/CustomWebResponseExceptionTranslator.java
@@ -2,6 +2,7 @@ package com.yukong.panda.auth.handler;
 
 import com.yukong.panda.auth.exception.CustomOauth2Exception;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.error.WebResponseExceptionTranslator;
@@ -19,6 +20,11 @@ public class CustomWebResponseExceptionTranslator implements WebResponseExceptio
     @Override
     public ResponseEntity<OAuth2Exception> translate(Exception e) throws Exception {
         log.error(e.getStackTrace().toString());
+        if (!(e instanceof OAuth2Exception)) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new CustomOauth2Exception(e.getMessage()));
+        }
         OAuth2Exception oAuth2Exception = (OAuth2Exception) e;
         return ResponseEntity
                 .status(oAuth2Exception.getHttpErrorCode())
