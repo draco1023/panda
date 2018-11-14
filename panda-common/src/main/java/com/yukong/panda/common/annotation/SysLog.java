5c2a97811ad2b98e09b19c40296dcc50bc46c452
YuKongEr <541130126@qq.com>
2018-11-14 14:29:51 +0800

    :art: 修改代码生成结构

---
+++ panda-common/src/main/java/com/yukong/panda/common/annotation/SysLog.java
@@ -0,0 +1,8 @@
+package com.yukong.panda.common.annotation;
+
+/**
+ * @author: yukong
+ * @date: 2018/11/14 11:21
+ */
+public @interface SysLog {
+}
---
+++ panda-common/src/main/java/com/yukong/panda/common/aop/LogAspect.java
@@ -0,0 +1,8 @@
+package com.yukong.panda.common.aop;
+
+/**
+ * @author: yukong
+ * @date: 2018/11/14 11:26
+ */
+public class LogAspect {
+}
