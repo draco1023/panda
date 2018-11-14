d448201b2ca1855faee8f08cb04dd635d1f60295
YuKongEr <541130126@qq.com>
2018-11-14 21:06:01 +0800

    :art: 删除多余文件

--- panda-auth/src/main/java/com/yukong/panda/auth/config/SecurityConfig.java
+++ panda-auth/src/main/java/com/yukong/panda/auth/config/SecurityConfig.java
@@ -1,13 +1,9 @@
 package com.yukong.panda.auth.config;
 
-import com.yukong.panda.auth.handler.PandaAuthenticationFailHandler;
-import com.yukong.panda.auth.handler.PandaAuthenticationSuccessHandler;
 import com.yukong.panda.auth.security.UserDetailsServiceImpl;
 import com.yukong.panda.common.config.IgnoreUrlPropertiesConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,12 +21,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private PandaAuthenticationSuccessHandler pandaAuthenticationSuccessHandler;
-
-    @Autowired
-    private PandaAuthenticationFailHandler pandaAuthenticationFailHandler;
 
     @Autowired
     private IgnoreUrlPropertiesConfig ignoreUrlPropertiesConfig;
@@ -56,11 +46,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-
-        http.
-                formLogin()
-                .successHandler(pandaAuthenticationSuccessHandler)
-                .failureHandler(pandaAuthenticationFailHandler);
         ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config
                 = http.requestMatchers().anyRequest()
                 .and()
